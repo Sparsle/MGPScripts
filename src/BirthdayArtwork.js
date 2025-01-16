6b5862a89996484dd3ff13b2b3367890613c009a
Sparsle <lzkJackie@hotmail.com>
2025-01-16 23:42:27 +0800

    Update BirthdayArtwork.js

--- src/BirthdayArtwork.js
+++ src/BirthdayArtwork.js
@@ -1,5 +1,5 @@
-const { CONFIG } = require('../utils/config.js');
-const { mw } = require('../utils/MediaWiki.js');
+const { CONFIG } = require('./utils/config.js');
+const { mw } = require('./utils/MediaWiki.js');
 
 const birthdayList = [];
 const today = new Date();
