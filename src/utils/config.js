bba1c37210bfb4ff71c3cbcd26a982cbecbc19b9
Sparsle <lzkJackie@hotmail.com>
2025-01-16 23:24:11 +0800

    Update config.js

--- src/utils/config.js
+++ src/utils/config.js
@@ -1,9 +1,7 @@
 import { env } from 'process';
 
-const CONFIG = {
+export const CONFIG = {
     PASSWORD: env.PASSWORD,
     SSO_USER_ID: env.SSO_USER_ID,
     SSO_TOKEN: env.SSO_TOKEN
 };
-
-export default CONFIG;
