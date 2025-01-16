7da3f908c01d7333a78c9b1adc560033d0628172
Sparsle <lzkJackie@hotmail.com>
2025-01-17 01:27:50 +0800

    Update config.js

--- src/utils/config.js
+++ src/utils/config.js
@@ -2,10 +2,10 @@ import { env } from 'process';
 
 const CONFIG = {
     USER_AGENT: env.USER_AGENT,
+    USERNAME: 'Chi_ZJ2@Bot',
     PASSWORD: env.PASSWORD,
     SSO_USER_ID: env.SSO_USER_ID,
     SSO_TOKEN: env.SSO_TOKEN,
-    TEST: env.TEST,
 };
 
 export default CONFIG;
