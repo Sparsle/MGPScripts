c90f217c8133f3f53b01b07195d6e98a77f58423
Sparsle <lzkJackie@hotmail.com>
2025-01-17 16:55:41 +0800

    Update BirthdayArtwork.js

--- src/BirthdayArtwork.js
+++ src/BirthdayArtwork.js
@@ -15,14 +15,11 @@ async function fetchPosts() {
     json.data.list.forEach((postItem) => {
         let title = postItem.post.subject;
         let postId = postItem.post.post_id;
-        let date = dateFormat(
-            new Date(parseInt(postItem.post.created_at) * 1000)
-        );
+        let date = dateFormat(new Date(parseInt(postItem.post.created_at) * 1000));
 
         if(dateFormat(today) != date) {
-            //return;
+            return;
         }
-
 
         let titleMatch = title.match(/(.+)生日快乐｜(.+)/);
         if(!titleMatch) {
@@ -51,47 +48,43 @@ async function addToWiki() {
 
     await api.login();
 
-    try {
-        let response = await api.post({
-            action: 'query',
-            prop: 'revisions',
-            titles: `原神/贺图`,
-            rvprop: 'content'
-        });
-        let code = response.query.pages[0].revisions[0].content;
+    let response = await api.post({
+        action: 'query',
+        prop: 'revisions',
+        titles: `原神/贺图`,
+        rvprop: 'content'
+    });
+    let code = response.query.pages[0].revisions[0].content;
 
-        response = await api.post({
-            action: 'query',
-            prop: 'revisions',
-            titles: `User:Chi_ZJ2/Data`,
-            rvprop: 'content'
-        });
-        let redirects = JSON.parse(response.query.pages[0].revisions[0].content);
+    response = await api.post({
+        action: 'query',
+        prop: 'revisions',
+        titles: `User:Chi_ZJ2/Data`,
+        rvprop: 'content'
+    });
+    let redirects = JSON.parse(response.query.pages[0].revisions[0].content);
 
-        birthdayList.forEach(([name, snippet]) => {
-            if(redirects[name] !== undefined) {
-                snippet = snippet.replace(`[[${name}]]`, `[[${redirects[name]}|${name}]]`);
-            }
+    birthdayList.forEach(([name, snippet]) => {
+        if(redirects[name] !== undefined) {
+            snippet = snippet.replace(`[[${name}]]`, `[[${redirects[name]}|${name}]]`);
+        }
 
-            code = code.replace(snippet + '\n', '');
-            code = code.replace(
-                /(角色生日 *==.+?<gallery>)/s,
-                `$1\n${snippet}`
-            );
-        });
+        code = code.replace(snippet + '\n', '');
+        code = code.replace(
+            /(角色生日 *==.+?<gallery>)/s,
+            `$1\n${snippet}`
+        );
+    });
 
-        let res = await api.post({
-            action: 'edit',
-            title: '原神/贺图',
-            text: code,
-            summary: '自动更新角色生日贺图',
-            bot: true,
-            tags: 'Bot',
-            token: await api.getToken('csrf', true)
-        });
-    } catch (err) {
-        console.error(err);
-    }
+    response = await api.post({
+        action: 'edit',
+        title: '原神/贺图',
+        text: code,
+        summary: '自动更新角色生日贺图',
+        bot: true,
+        tags: 'Bot',
+        token: await api.getToken('csrf', true)
+    });
 
     await api.logout();
 }
