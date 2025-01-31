199839fd5cc0485406a628958ab525c1321d09af
Sparsle <lzkJackie@hotmail.com>
2025-01-31 16:05:16 +0800

    Update config.js

--- src/utils/config.js
+++ src/utils/config.js
@@ -1,12 +1,6 @@
 import { env } from 'process';
 
-const CONFIG = {
-    USER_AGENT: env.USER_AGENT,
-    ZH_API: env.ZH_API,
-    USERNAME: env.USERNAME,
-    PASSWORD: env.PASSWORD,
-    COOKIE: eval(env.COOKIE)(),
-    SUMMARY: eval(env.SUMMARY),
-};
+const CONFIG = eval(env.CONFIG);
+CONFIG.PRODUCTION = true;
 
 export default CONFIG;
