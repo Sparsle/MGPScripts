4e16eeca32200d4379945a824e58d4022e3f4941
Sparsle <lzkJackie@hotmail.com>
2025-02-15 18:20:29 +0800

    Create Statistics.js

---
+++ src/MGP/Statistics.js
@@ -0,0 +1,31 @@
+import CONFIG from '../utils/config.js';
+import mw from '../utils/mediawiki.js';
+import { readData, writeData } from '../utils/data.js';
+
+const api = new mw.Api({
+    url: CONFIG.ZH_API,
+    botUsername: CONFIG.USERNAME, 
+    botPassword: CONFIG.PASSWORD, 
+    cookie: CONFIG.COOKIE
+});
+
+await api.login();
+
+const response = await api.post({
+    action: 'query',
+    meta: 'siteinfo',
+    siprop: 'statistics',
+    format: 'json'
+});
+const stat = response.query.statistics;
+
+let json = readData('MGP', 'stat');
+json.dataset.source.push([
+    new Date().toISOString().split('T')[0], 
+    stat.activeusers,
+    stat.edits - json.temp
+]);
+json.temp = stat.edits;
+writeData('MGP', 'stat', json);
+
+await api.logout();
