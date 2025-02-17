59ef746ae66d7aa9e97463eeee7a41733c002dda
Sparsle <lzkJackie@hotmail.com>
2025-02-17 22:43:13 +0800

    Update Statistics.js

--- src/MGP/Statistics.js
+++ src/MGP/Statistics.js
@@ -19,6 +19,7 @@ const response = await api.post({
 });
 const stat = response.query.statistics;
 console.log(stat);
+await new Promise(resolve => setTimeout(resolve, 30000));
 
 let json = readData('MGP', 'stat');
 json.dataset.source.push([
@@ -28,5 +29,14 @@ json.dataset.source.push([
 ]);
 json.temp = stat.edits;
 writeData('MGP', 'stat', json);
+
+response = await api.post({
+    action: 'edit',
+    title: `User:${CONFIG.USERNAME.slice(0, -4)}/statistics.json`,
+    text: JSON.stringify(json).replace(/"temp":.+?,/, ""),
+    bot: true,
+    tags: 'Bot',
+    token: await api.getToken('csrf', true)
+}).then(console.log);
 
 await api.logout();
